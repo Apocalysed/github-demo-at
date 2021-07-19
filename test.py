086d33bfd8128d84d8a8a0d65564b78af22da44f
Apocalysed <phamhuynhanhtien.12a20.2019@gmail.com>
2021-07-19 10:41:40 +0700

    Add file

---
+++ test.py
@@ -0,0 +1,8 @@
+import re
+#1
+str = 'Hai khu chung cư nằm trong khu vực "phố chung cư" ở Đà Nẵng. Các ca dương tính ghi nhận tại đây gồm "bệnh nhân 849, 853, 780, 781, 797 và 802". Từ 25/7 đến nay, Đà Nẵng đã ghi nhận 296 người mắc nCoV. Thành phố đang tiếp tục áp dụng cách ly xã hội theo chỉ thỉ 16 để khoanh vùng, dập dịch.'
+a = re.findall(r'[\w+\d\sa-zA-Z0-9]+[.,""]',str)
+print(a)
+#2
+def checkToken:
+    
